60c0eafa63b6cd7ab555a6a75fc572eb61c02f6e
Santiago Roussineau <101692734+roussineau@users.noreply.github.com>
2025-09-06 19:56:40 -0300

    Para mergear

--- src/Util.hs
+++ src/Util.hs
@@ -1,18 +1,15 @@
 module Util where
 
--- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
--- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+
 alinearDerecha :: Int -> String -> String
--- cb : length s >= n = s
---alinearDerecha n s = foldr (aca va la funcion) (s) (lista a recorrer, s)
-alinearDerecha n s = foldr s s n 
+alinearDerecha n s = foldr (\_ r -> if n - length r <= 0 then s else ' ' : r) s [1..n - length s]
 
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f xs = error "COMPLETAR EJERCICIO 2"
+actualizarElem n f = zipWith (\i x -> if i == n then f x else x) [0 ..]
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
